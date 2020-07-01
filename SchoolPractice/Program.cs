709c691c3c5cf82d8ac4485f16377233fcad7628
Yiwei Zhao <yiweizhao15@gmail.com>
2020-06-30 21:18:42 -0500

    Ch5 exercised solution added.

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,6 +7,32 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Test your exercise solutions with print statements here.
+            Student testStudent= new Student("Test");
+            testStudent.NumberOfCredits = 3;
+            testStudent.Gpa = 3.0;
+            Student extraStudent = testStudent;
+            //testStudent.ToString();
+            Student otherStudent = new Student("Other");
+            Console.WriteLine(testStudent);
+            Console.WriteLine(otherStudent);
+
+            Console.WriteLine(testStudent.Equals(otherStudent));
+            Console.WriteLine(testStudent.Equals(extraStudent));
+
+            Course newCourse = new Course();
+            Console.WriteLine(testStudent.Equals(newCourse));
+
+
+            //Test AddGrade method
+            int newCourseCredit;
+            double newGrade;
+
+            newCourseCredit = 3;
+            newGrade = 4.0;
+
+            testStudent.AddGrade(newCourseCredit, newGrade);
+            Console.WriteLine(testStudent);
+
         }
     }
 }
--- SchoolPractice/Student.cs
+++ SchoolPractice/Student.cs
@@ -12,10 +12,10 @@ namespace SchoolPractice
         public Student(string name, int studentId,
             int numberOfCredits, double gpa)
         {
-            Name = name;
-            StudentId = studentId;
-            NumberOfCredits = numberOfCredits;
-            Gpa = gpa;
+            this.Name = name;
+            this.StudentId = studentId;
+            this.NumberOfCredits = numberOfCredits;
+            this.Gpa = gpa;
         }
 
         public Student(string name, int studentId)
@@ -28,23 +28,82 @@ namespace SchoolPractice
         }
 
         // TODO: Complete the AddGrade method.
+
+       
         public void AddGrade(int courseCredits, double grade)
         {
             // Update the appropriate properties: NumberOfCredits, Gpa
+            double currentQualityScore = Gpa * NumberOfCredits;
+            double newQualityScore = courseCredits * grade;
+            double totalQualityScore = currentQualityScore + newQualityScore;
+            NumberOfCredits = NumberOfCredits + courseCredits;
+            Gpa = totalQualityScore / NumberOfCredits;
         }
 
         //TODO: Complete the GetGradeLevel method here:
         public string GetGradeLevel(int credits)
         {
             // Determine the grade level of the student based on NumberOfCredits
-            return "grade level tbd";
+
+            if (credits >= 90)
+            {
+                return "Senior";
+            }
+
+            else if (credits >= 60)
+            {
+                return "Junior";
+            }
+
+            else if (credits >= 30)
+            {
+                return "Sophomore";
+            }
+
+            else if (credits <=29)
+            {
+                return "Freshman";
+            } 
+
+            else
+            {
+                return "Invalid Credits!";
+            }
+            
+          // return "grade level tbd";
         }
 
         // TODO: Add your custom 'ToString' method here. Make sure it returns a well-formatted string rather
         //  than just the class fields.
 
+        public override string ToString()
+        {
+            return "Name: " + Name  + "\nStudent ID: " + StudentId + "\nCredits: " + NumberOfCredits 
+                + ", GPA: " + Gpa + ";" + "\nGrade Level: " + GetGradeLevel(NumberOfCredits);
+        }
+
         // TODO: Add your custom 'Equals' method here. Consider which fields should match in order to call two
         //  Student objects equal.
 
+       public override bool Equals(object obj)
+        {
+            if (obj == this)
+            {
+                return true;
+            }
+
+            if (obj == null)
+            {
+                return false;
+            }
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            Student toCompare = obj as Student;
+
+            return this.Name == toCompare.Name && this.StudentId == toCompare.StudentId;
+        }
     }
 }
